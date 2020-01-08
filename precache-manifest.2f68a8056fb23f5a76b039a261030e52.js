e324552a407545c78999f97998ea57d066824c1f
unknown <emeraldjunn@hotmail.com>
2020-01-08 15:53:22 +0400

    Updates

---
+++ precache-manifest.2f68a8056fb23f5a76b039a261030e52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "160b2c609a6ce9b0ef3f5793dfdb8303",
+    "url": "/monsterss-rolodex/index.html"
+  },
+  {
+    "revision": "e47d1dba4dd5f332a74b",
+    "url": "/monsterss-rolodex/static/css/main.bd2f044a.chunk.css"
+  },
+  {
+    "revision": "c728ba4a2366e885ec54",
+    "url": "/monsterss-rolodex/static/js/2.d8d20882.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsterss-rolodex/static/js/2.d8d20882.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e47d1dba4dd5f332a74b",
+    "url": "/monsterss-rolodex/static/js/main.8191780c.chunk.js"
+  },
+  {
+    "revision": "c9c8dc4b7bfb49ed1b8b",
+    "url": "/monsterss-rolodex/static/js/runtime-main.350dc284.js"
+  }
+]);
